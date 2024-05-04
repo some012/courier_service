005203909bbed3f36cab8e2e7a27d91e85b4d914
some_012 <someone200315@gmail.com>
2024-05-05 00:05:02 +0700

    Add some features

--- app/utilities/crud.py
+++ app/utilities/crud.py
@@ -17,7 +17,7 @@ def average_order_completion_time(db, model, model_courier):
         average_completion_time_minutes = total_completion_time / (num_completed_orders * 60)
         return "{:.2f}".format(average_completion_time_minutes)
     else:
-        return None
+        return 0
 
 
 def average_order_days(db, model, model_courier):
@@ -26,7 +26,8 @@ def average_order_days(db, model, model_courier):
         .filter(model.courier_id == model_courier.id) \
         .group_by(func.date_trunc('day', model.date_end)) \
         .all()
-
+    if daily_order_count is None:
+        return 0
     # Вычисляем среднее количество заказов в день
     total_orders = len(daily_order_count)
     total_order_count = sum(count for count, in daily_order_count)
@@ -34,4 +35,4 @@ def average_order_days(db, model, model_courier):
         average_orders_per_day = total_order_count / total_orders
         return int(average_orders_per_day)
     else:
-        return None
+        return 0
